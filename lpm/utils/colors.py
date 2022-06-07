e01f14ea9222a57d0953aacf1ad2fc316a439220
Mark <markhappy200@gmail.com>
2022-06-07 17:31:56 +0300

    Added utils

---
+++ lpm/utils/colors.py
@@ -0,0 +1,30 @@
+from math import sqrt
+
+COLORS = [
+    (255, 255, 255),  # 0 white
+    (225, 190, 255),  # 1 light purple
+    (225, 110, 255),  # 2 purple
+    (230, 0, 0),  # 3 red
+    (255, 80, 0),  # 4 orange
+    (255, 255, 0),  # 5 yellow
+    (50, 140, 0),  # 6 green
+    (0, 150, 210),  # 7 light blue
+    (0, 75, 180),  # 8 blue
+    (0, 35, 115),  # 9 dark blue
+    (128, 128, 128),  # 10 light grey
+    (77, 77, 77),  # 11 dark grey
+    (0, 0, 0),  # 12 black
+]
+
+HI_P = COLORS[:4]
+MD_P = COLORS[4:9]
+LO_P = COLORS[9:]
+
+def match_color(rgb: list) -> tuple:
+    r, g, b = rgb
+    color_diffs = []
+    for color in COLORS:
+        cr, cg, cb = color
+        color_diff = sqrt(abs(r - cr) ** 2 + abs(g - cg) ** 2 + abs(b - cb) ** 2)
+        color_diffs.append((color_diff, color))
+    return min(color_diffs)[1]
---
+++ lpm/utils/weather.py
@@ -0,0 +1,20 @@
+import os, json, datetime, requests
+
+API_URL = "https://api.openweathermap.org/data/2.5/forecast?units=metric&lat={}&lon={}&appid={}"
+
+def get_coords_weather(coords: tuple) -> list:
+    req_url = API_URL.format(coords[0], coords[1], os.getenv('WEATHER_KEY'))
+    response = json.loads(requests.get(req_url).content)
+    sunrise = datetime.fromtimestamp(response["city"]['sunrise']).hour
+    sunset = datetime.fromtimestamp(response["city"]['sunset']).hour
+    data = []
+    for item in response["list"]:
+        hour = datetime.fromtimestamp(item["dt"]).hour
+        if hour > sunset + 1 or hour < sunrise - 1:
+            time = item["dt_txt"]
+            clouds = item["clouds"]["all"]
+            temperature = item["main"]["temp"]
+            pressure = item["main"]["pressure"]
+            humidity = item["main"]["humidity"]
+            data.append([time, clouds, temperature, pressure, humidity])
+    return sorted(data, key = lambda x: x[1])[0]
